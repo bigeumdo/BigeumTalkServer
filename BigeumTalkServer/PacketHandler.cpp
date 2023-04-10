7017210fa70a60da79cd96c0313c78535a836491
bigeumdo <bigeumdoisland@gmail.com>
2023-04-10 22:44:55 +0900

    Refactor: 패킷 처리 작업 Protobuf 기반으로 변경

--- BigeumTalkServer/PacketHandler.cpp
+++ BigeumTalkServer/PacketHandler.cpp
@@ -5,178 +5,32 @@
 #include "Service.h"
 #include <chrono>
 
+unordered_map<unsigned short, PacketHandlerFunc> GPacketHandler;
 
-/**
- * \brief 도착한 패킷의 프로토콜을 구분하여 핸들러를 호출하는 함수
- * \param session 패킷을 보낸 세션 shared_ptr 주소
- * \param buffer 패킷이 담긴 버퍼 주소
- * \param len 패킷의 길이
- * \param protocol 패킷의 프로토콜
- */
-void PacketHandler::HandlePacket(shared_ptr<Session>& session, BYTE* buffer, int len, unsigned short protocol)
+bool Handle_C_LOGIN(shared_ptr<Session>& session, Protocol::C_LOGIN& pkt)
 {
-	switch (protocol)
-	{
-	case C_VERSION_CHECK:
-		break;
-	case C_LOGIN:
-		Handle_C_LOGIN(session, buffer, len);
-		break;
-	case C_ENTER_ROOM:
-		Handle_C_ENTER_ROOM(session, buffer, len);
-		break;
-	case C_CHAT:
-		Handle_C_CHAT(session, buffer, len);
-		break;
-	}
-}
-
-
-/**
- * \brief S_LOGIN 프로토콜의 버퍼를 생성하는 함수
- * \param resultCode 서버 처리 결과 코드
- * \param userId User의 ID
- * \return 생성된 버퍼
- */
-shared_ptr<SendBuffer> PacketHandler::MakeBuffer_S_LOGIN(unsigned short resultCode, unsigned long long userId)
-{
-	Protocol::S_LOGIN pkt;
-	pkt.resultCode = resultCode;
-	pkt.userId = userId;
-
-	return MakeSendBuffer(pkt, S_LOGIN);
-}
-
-
-/**
- * \brief S_ENTER_ROOM 프로토콜의 버퍼를 생성하는 함수
- * \param resultCode 서버 처리 결과 코드
- * \param user User 객체 shared_ptr
- * \return 생성된 버퍼
- */
-shared_ptr<SendBuffer> PacketHandler::MakeBuffer_S_ENTER_ROOM(unsigned short resultCode, shared_ptr<User> user)
-{
-	Protocol::S_ENTER_ROOM pkt{0,};
-	pkt.resultCode = resultCode;
-
-	if (pkt.resultCode == ENTER_ROOM_SUCCESS)
-	{
-		pkt.roomId = user->room->GetRoomId();
-		pkt.roomName = user->room->GetRoomName();
-	}
-	else
-	{
-		return MakeSendBuffer(pkt, S_ENTER_ROOM);
-	}
-
-	// 채팅방에 있는 유저 리스트
-	pkt.users.reserve(user->room->GetUsersList().size());
-
-	auto& users = user->room->GetUsersList();
-	for (auto& user : users)
-	{
-		pkt.users.push_back({user.first, user.second->nickname});
-	}
-
-	return MakeSendBuffer(pkt, S_ENTER_ROOM);
-}
-
-
-/**
- * \brief S_CHAT 프로토콜의 버퍼를 생성하는 함수
- * \param resultCode 서버 처리 결과 코드
- * \param cPkt 클라이언트에서 보낸 패킷
- * \return 생성된 버퍼
- */
-shared_ptr<SendBuffer> PacketHandler::MakeBuffer_S_CHAT(unsigned short resultCode, Protocol::C_CHAT cPkt)
-{
-	Protocol::S_CHAT pkt;
-	{
-		pkt.serverMessage = false;
-		pkt.userId = 0;
-		pkt.nickname = "";
-		pkt.message = "";
-		pkt.timestamp = 0;
-	}
-
-	pkt.resultCode = resultCode;
-	if (resultCode == CHAT_OK)
-	{
-		pkt.nickname = cPkt.nickname;
-		pkt.userId = cPkt.userId;
-		pkt.message = cPkt.message;
-		pkt.serverMessage = false;
-		pkt.timestamp = std::chrono::duration_cast<chrono::seconds>(chrono::system_clock::now().time_since_epoch()).
-			count();
-	}
-
-	return MakeSendBuffer(pkt, S_CHAT);
-}
-
-
-/**
- * \brief S_OTHER_ENTER 프로토콜의 버퍼를 생성하는 함수
- * \param other 채팅방에 들어온 유저
- * \return 생성된 버퍼
- */
-shared_ptr<SendBuffer> PacketHandler::MakeBuffer_S_OTHER_ENTER(shared_ptr<User> other)
-{
-	Protocol::S_OTHER_ENTER pkt;
-	pkt.user.nickName = other->nickname;
-	pkt.user.userId = other->userId;
-	pkt.timestamp = std::chrono::duration_cast<chrono::seconds>(chrono::system_clock::now().time_since_epoch()).
-		count();
-
-	return MakeSendBuffer(pkt, S_OTHER_ENTER);
-}
-
-
-/**
- * \brief S_OTHER_LEAVE 프로토콜의 버퍼를 생성하는 함수
- * \param other 채팅방에서 퇴장한 유저
- * \return 생성된 버퍼
- * \details RoomManager 클래스의 LeaveRoom함수에서 호출됨
- */
-shared_ptr<SendBuffer> PacketHandler::MakeBuffer_S_OTHER_LEAVE(shared_ptr<User> other)
-{
-	Protocol::S_OTHER_LEAVE pkt;
-	pkt.user.nickName = other->nickname;
-	pkt.user.userId = other->userId;
-	pkt.timestamp = std::chrono::duration_cast<chrono::seconds>(chrono::system_clock::now().time_since_epoch()).
-		count();
-
-	return MakeSendBuffer(pkt, S_OTHER_LEAVE);
-}
-
-
-/**
- * \brief C_LOGIN 프로토콜을 처리하는 함수
- * \param session C_LOGIN 프로토콜 패킷을 전달한 세션
- * \param buffer 패킷이 담긴 버퍼 주소
- * \param len 패킷의 길이
- */
-void Handle_C_LOGIN(shared_ptr<Session>& session, BYTE* buffer, int len)
-{
-	Protocol::C_LOGIN pkt;
-	pkt.GetPacket(buffer, len);
-
 	auto service = session->GetService();
+	Protocol::S_LOGIN sPkt;
 
 	// 닉네임 사용 등록 중복 닉네임 확인
-	if (service->UseNickname(pkt.nickname) == false)
+	if (service->UseNickname(pkt.user().nickname()) == false)
 	{
-		shared_ptr<SendBuffer> sendBuffer = PacketHandler::MakeBuffer_S_LOGIN(LOGIN_EXIST);
+		sPkt.set_success(false);
+		sPkt.set_serverid(0);
+
+		shared_ptr<SendBuffer> sendBuffer = PacketHandler::MakeBuffer_S_LOGIN(sPkt);
 		session->Send(sendBuffer);
-		return;
+
+		return true;
 	}
 
-	static atomic<unsigned long long> idGenerator = 1;
+	static atomic<unsigned long long> idGenerator = 0;
 
 	// User 객체 생성 후 Session이 참조
 	// Cycle이 형성되므로 주의
 	auto userRef = make_shared<User>();
-	userRef->userId = idGenerator++;
-	userRef->nickname = pkt.nickname;
+	userRef->userId = ++idGenerator;
+	userRef->nickname = pkt.user().nickname();
 	userRef->ownerSession = session;
 
 	session->_user = userRef;
@@ -185,58 +39,86 @@ void Handle_C_LOGIN(shared_ptr<Session>& session, BYTE* buffer, int len)
 	cout << "[USER LOGIN] " << '[' << userRef->userId << "] " << userRef->nickname << endl;
 #endif
 
-	shared_ptr<SendBuffer> sendBuffer = PacketHandler::MakeBuffer_S_LOGIN(LOGIN_SUCCESS, userRef->userId);
+	sPkt.set_success(true);
+	sPkt.set_serverid(userRef->userId);
+	shared_ptr<SendBuffer> sendBuffer = PacketHandler::MakeBuffer_S_LOGIN(sPkt);
 	session->Send(sendBuffer);
+
+	return true;
 }
 
-
-/**
- * \brief C_ENTER_ROOM 프로토콜을 처리하는 함수
- * \param session C_ENTER_ROOM 프로토콜 패킷을 전달한 세션
- * \param buffer 패킷이 담긴 버퍼 주소
- * \param len 패킷의 길이
- */
-void Handle_C_ENTER_ROOM(shared_ptr<Session>& session, BYTE* buffer, int len)
+bool Handle_C_CREATE_ROOM(shared_ptr<Session>& session, Protocol::C_CREATE_ROOM& pkt)
 {
-	Protocol::C_ENTER_ROOM pkt;
-	pkt.GetPacket(buffer, len);
+	return true;
+}
 
+bool Handle_C_ENTER_ROOM(shared_ptr<Session>& session, Protocol::C_ENTER_ROOM& pkt)
+{
 	auto service = session->GetService();
-	if (service->GetRoomManager()->EnterRoom(session->_user, pkt.roomId) == false)
+	Protocol::S_ENTER_ROOM sPkt;
+
+	if (service->GetRoomManager()->EnterRoom(session->_user, pkt.roomid()) == false)
 	{
-		shared_ptr<SendBuffer> sendBuffer = PacketHandler::MakeBuffer_S_ENTER_ROOM(
-			ENTER_ROOM_FAIL, session->_user);
+		sPkt.set_success(false);
+		sPkt.set_isfail(true);
+
+		shared_ptr<SendBuffer> sendBuffer = PacketHandler::MakeBuffer_S_ENTER_ROOM(sPkt);
 		session->Send(sendBuffer);
-		return;
+		return true;
 	}
 
 	// 입장 성공 - 시도한 유저에게
-	shared_ptr<SendBuffer> sendBuffer = PacketHandler::MakeBuffer_S_ENTER_ROOM(
-		ENTER_ROOM_SUCCESS, session->_user);
+	sPkt.set_success(true);
+	auto roomData = new Protocol::S_ENTER_ROOM::RoomData();
+	roomData->set_name(session->_user->room->GetRoomName());
+
+	auto roomUsers = session->_user->room->GetUsersList();
+	for (auto& user : roomUsers)
+	{
+		auto roomUser = roomData->add_users();
+		roomUser->set_id(user.first);
+		roomUser->set_nickname(user.second->nickname);
+	}
+	sPkt.set_allocated_roomdata(roomData);
+
+	shared_ptr<SendBuffer> sendBuffer = PacketHandler::MakeBuffer_S_ENTER_ROOM(sPkt);
 	session->Send(sendBuffer);
 	sendBuffer = nullptr;
+
+	return true;
 }
 
-
-/**
- * \brief C_CHAT 프로토콜을 처리하는 함수
- * \param session C_CHAT 프로토콜 패킷을 전달한 세션
- * \param buffer 패킷이 담긴 버퍼 주소
- * \param len 패킷의 길이
- */
-void Handle_C_CHAT(shared_ptr<Session>& session, BYTE* buffer, int len)
+bool Handle_C_LEAVE_ROOM(shared_ptr<Session>& session, Protocol::C_LEAVE_ROOM& pkt)
 {
-	Protocol::C_CHAT pkt;
-	pkt.GetPacket(buffer, len);
+	return true;
+}
 
+bool Handle_C_ROOM_LIST(shared_ptr<Session>& session, Protocol::C_ROOM_LIST& pkt)
+{
+	return true;
+}
+
+bool Handle_C_CHAT(shared_ptr<Session>& session, Protocol::C_CHAT& pkt)
+{
 	auto room = session->_user->room;
 	if (room == nullptr)
 	{
-		shared_ptr<SendBuffer> sendBuffer = PacketHandler::MakeBuffer_S_CHAT(CHAT_FAIL, pkt);
-		session->Send(sendBuffer);
-		return;
+		// 비 정상적인 상황
+		// TODO Error 패킷 전송
+		return false;
 	}
 
-	shared_ptr<SendBuffer> sendBuffer = PacketHandler::MakeBuffer_S_CHAT(CHAT_OK, pkt);
+	Protocol::S_CHAT sPkt;
+	Protocol::User user;
+	user.set_id(session->_user->userId);
+	user.set_nickname(session->_user->nickname);
+	sPkt.set_allocated_user(&user);
+	sPkt.set_msg(pkt.msg());
+	sPkt.set_timestamp(std::chrono::duration_cast<chrono::seconds>(chrono::system_clock::now().time_since_epoch()).
+		count());
+
+	shared_ptr<SendBuffer> sendBuffer = PacketHandler::MakeBuffer_S_CHAT(sPkt);
 	room->Broadcast(sendBuffer);
+
+	return true;
 }
--- BigeumTalkServer/PacketHandler.h
+++ BigeumTalkServer/PacketHandler.h
@@ -1,57 +1,172 @@
 ﻿#pragma once
-#include "Protocol.h"
+#include "Protocol.pb.h"
+
+// TODO 자동화
 
 class Session;
+using PacketHandlerFunc = std::function<bool(shared_ptr<Session>&, BYTE*, int)>;
+extern unordered_map<unsigned short, PacketHandlerFunc> GPacketHandler;
+
+enum : unsigned short
+{
+	P_C_LOGIN = 1000,
+	P_S_LOGIN,
+	P_C_CREATE_ROOM,
+	P_S_CREATE_ROOM,
+	P_C_ENTER_ROOM,
+	P_S_ENTER_ROOM,
+	P_C_LEAVE_ROOM,
+	P_S_LEAVE_ROOM,
+	P_C_ROOM_LIST,
+	P_S_ROOM_LIST,
+	P_C_CHAT,
+	P_S_CHAT,
+	P_S_OTHER_ENTER,
+	P_S_OTHER_LEAVE
+};
+
+/*
+ * Handle_C_ 함수 정책
+ * 비 정상적인 패킷에 한해서 false 반환
+ */
+
+bool Handle_C_LOGIN(shared_ptr<Session>& session, Protocol::C_LOGIN& pkt);
+bool Handle_C_CREATE_ROOM(shared_ptr<Session>& session, Protocol::C_CREATE_ROOM& pkt);
+bool Handle_C_ENTER_ROOM(shared_ptr<Session>& session, Protocol::C_ENTER_ROOM& pkt);
+bool Handle_C_LEAVE_ROOM(shared_ptr<Session>& session, Protocol::C_LEAVE_ROOM& pkt);
+bool Handle_C_ROOM_LIST(shared_ptr<Session>& session, Protocol::C_ROOM_LIST& pkt);
+bool Handle_C_CHAT(shared_ptr<Session>& session, Protocol::C_CHAT& pkt);
 
 /**
  * \brief ServerPacketHandler 클래스
- * \details 서버에 도착한 패킷을 처리하고 클라이언트에게 보낼 패킷을 생성합니다.
+ * \details 서버에 도착한 패킷을 처리하고 클라이언트에게 보낼 데이터를 생성합니다.
  */
 class PacketHandler
 {
 public:
-	static void HandlePacket(shared_ptr<Session>& session, BYTE* buffer, int len, unsigned short protocol);
+	/**
+	 * \brief 정의한 패킷 처리 함수를 초기화하는 함수
+	 */
+	static void Init()
+	{
+		GPacketHandler.emplace(P_C_LOGIN, [](shared_ptr<Session>& session, BYTE* buffer, int len) -> bool
+		{
+			return HandlePacketTemplate<Protocol::C_LOGIN>(Handle_C_LOGIN, session, buffer, len);
+		});
+		GPacketHandler.emplace(P_C_ENTER_ROOM, [](shared_ptr<Session>& session, BYTE* buffer, int len) -> bool
+		{
+			return HandlePacketTemplate<Protocol::C_ENTER_ROOM>(Handle_C_ENTER_ROOM, session, buffer, len);
+		});
+		GPacketHandler.emplace(P_C_LEAVE_ROOM, [](shared_ptr<Session>& session, BYTE* buffer, int len) -> bool
+		{
+			return HandlePacketTemplate<Protocol::C_LEAVE_ROOM>(Handle_C_LEAVE_ROOM, session, buffer, len);
+		});
+		GPacketHandler.emplace(P_C_ROOM_LIST, [](shared_ptr<Session>& session, BYTE* buffer, int len) -> bool
+		{
+			return HandlePacketTemplate<Protocol::C_ROOM_LIST>(Handle_C_ROOM_LIST, session, buffer, len);
+		});
+		GPacketHandler.emplace(P_C_CHAT, [](shared_ptr<Session>& session, BYTE* buffer, int len) -> bool
+		{
+			return HandlePacketTemplate<Protocol::C_CHAT>(Handle_C_CHAT, session, buffer, len);
+		});
+	}
 
-	static shared_ptr<SendBuffer> MakeBuffer_S_LOGIN(unsigned short resultCode, unsigned long long userId = 0);
-	static shared_ptr<SendBuffer> MakeBuffer_S_ENTER_ROOM(unsigned short resultCode, shared_ptr<User> user);
-	static shared_ptr<SendBuffer> MakeBuffer_S_CHAT(unsigned short resultCode, Protocol::C_CHAT cPkt);
-	static shared_ptr<SendBuffer> MakeBuffer_S_OTHER_ENTER(shared_ptr<User> other);
-	static shared_ptr<SendBuffer> MakeBuffer_S_OTHER_LEAVE(shared_ptr<User> other);
 
-	template <typename T>
-	static shared_ptr<SendBuffer> MakeSendBuffer(T& pkt, unsigned short pktId);
+	/**
+	 * \brief 도착한 패킷을 처리하는 함수
+	 * \param session 패킷을 Recv한 Session
+	 * \param buffer Session의 Buffer
+	 * \param len 패킷의 길이
+	 * \return 정상 처리 여부
+	 */
+	static bool HandlePacket(shared_ptr<Session>& session, BYTE* buffer, int len)
+	{
+		auto header = reinterpret_cast<PacketHeader*>(buffer);
+		auto handler = GPacketHandler.find(header->id);
+		if (handler == GPacketHandler.end())
+		{
+			return false;
+		}
+		return handler->second(session, buffer, len);
+	}
+
+	static shared_ptr<SendBuffer> MakeBuffer_S_LOGIN(Protocol::S_LOGIN& pkt)
+	{
+		return MakeSendBuffer(pkt, P_S_LOGIN);
+	}
+
+	static shared_ptr<SendBuffer> MakeBuffer_S_ENTER_ROOM(Protocol::S_ENTER_ROOM& pkt)
+	{
+		return MakeSendBuffer(pkt, P_S_ENTER_ROOM);
+	}
+
+	static shared_ptr<SendBuffer> MakeBuffer_S_LEAVE_ROOM(Protocol::S_LEAVE_ROOM& pkt)
+	{
+		return MakeSendBuffer(pkt, P_S_LEAVE_ROOM);
+	}
+
+	static shared_ptr<SendBuffer> MakeBuffer_S_CHAT(Protocol::S_CHAT& pkt)
+	{
+		return MakeSendBuffer(pkt, P_S_CHAT);
+	}
+
+	static shared_ptr<SendBuffer> MakeBuffer_S_OTHER_ENTER(Protocol::S_OTHER_ENTER& pkt)
+	{
+		return MakeSendBuffer(pkt, P_S_OTHER_ENTER);
+	}
+
+	static shared_ptr<SendBuffer> MakeBuffer_S_OTHER_LEAVE(Protocol::S_OTHER_LEAVE& pkt)
+	{
+		return MakeSendBuffer(pkt, P_S_OTHER_LEAVE);
+	}
+
+private:
+	/**
+	 * \brief Init을 통해 map에 정의한 패킷 처리 함수를 등록
+	 * \tparam PacketType 정의한 패킷
+	 * \tparam HandleFunc 패킷 처리 함수
+	 * \param func 패킷 처리 함수
+	 * \param session 패킷을 Recv한 Session
+	 * \param buffer Session의 버퍼
+	 * \param len 패킷의 길이
+	 * \return 정상 처리 여부
+	 */
+	template <typename PacketType, typename HandleFunc>
+	static bool HandlePacketTemplate(HandleFunc func, shared_ptr<Session>& session, BYTE* buffer, int len)
+	{
+		PacketType pkt;
+		const int headerSize = sizeof(PacketHeader);
+		if (pkt.ParseFromArray(buffer + headerSize, len - headerSize) == false)
+		{
+			return false;
+		}
+
+		return func(session, pkt);
+	}
+
+
+	/**
+	 * \brief SendBuffer에 정의한 패킷 객체를 직렬화하여 담는 함수
+	 * \tparam PacketType 정의한 패킷
+	 * \param pkt 정의한 패킷
+	 * \param pktId 프로토콜 ID
+	 * \return 직렬화된 내용이 담긴 버퍼
+	 */
+	template <typename PacketType>
+	static shared_ptr<SendBuffer> MakeSendBuffer(PacketType& pkt, unsigned short pktId)
+	{
+		const unsigned short dataSize = static_cast<unsigned short>(pkt.ByteSizeLong());
+		const unsigned short packetSize = dataSize + sizeof(PacketHeader);
+
+		shared_ptr<SendBuffer> sendBuffer = GSendBufferManager->Open(packetSize);
+		auto header = reinterpret_cast<PacketHeader*>(sendBuffer->Buffer());
+		header->id = pktId;
+		header->size = packetSize;
+
+		pkt.SerializeToArray(&header[1], dataSize);
+
+		sendBuffer->Close(packetSize);
+
+		return sendBuffer;
+	}
 };
-
-
-/**
- * \brief 생성된 패킷이 담긴 버퍼를 반환하는 함수
- * \tparam T 서버에서 보낼 패킷 정의 구조체
- * \param pkt 패킷으로 생성할 구조체
- * \param pktId 패킷 프로토콜 ID
- * \return 생성 완료된 버퍼 shared_ptr
- */
-template <typename T>
-shared_ptr<SendBuffer> PacketHandler::MakeSendBuffer(T& pkt, unsigned short pktId)
-{
-	// 보낼 데이터를 string 자료형의 json으로 변환 -> wstring으로 변환
-	wstring pktBody = S2W(pkt.MakeJson());
-
-	const unsigned short dataSize = pktBody.length() * sizeof(WCHAR);
-	const unsigned short packetSize = dataSize + sizeof(PacketHeader);
-
-	shared_ptr<SendBuffer> sendBuffer = GSendBufferManager->Open(packetSize);
-	auto header = reinterpret_cast<PacketHeader*>(sendBuffer->Buffer());
-
-	header->size = packetSize;
-	header->id = pktId;
-
-	// 헤더의 다음 위치에 패킷 내용 memcpy
-	memcpy(&header[1], pktBody.data(), dataSize);
-	sendBuffer->Close(packetSize);
-
-	return sendBuffer;
-}
-
-void Handle_C_LOGIN(shared_ptr<Session>& session, BYTE* buffer, int len);
-void Handle_C_ENTER_ROOM(shared_ptr<Session>& session, BYTE* buffer, int len);
-void Handle_C_CHAT(shared_ptr<Session>& session, BYTE* buffer, int len);
