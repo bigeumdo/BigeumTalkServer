f70881e4a79285d5fca28b2fc7ea6a1c0e4f5618
bigeumdo <bigeumdoisland@gmail.com>
2023-04-13 20:49:29 +0900

    Refactor: Protobuf 파이썬 생성 제거

--- Protobuf/GenProtocol.bat
+++ Protobuf/GenProtocol.bat
@@ -2,13 +2,12 @@ pushd %~dp0
 
 protoc.exe -I=./ --cpp_out=./ ./Protocol.proto
 protoc.exe --js_out=import_style=commonjs,binary:. ./Protocol.proto
-protoc.exe --python_out=pyi_out:./ ./Protocol.proto
 
 
 XCOPY /Y Protocol.pb.cc "../BigeumTalkServer"
 XCOPY /Y Protocol.pb.h "../BigeumTalkServer"
 
-XCOPY /Y Protocol_pb2.py "../DummyClient"
-XCOPY /Y Protocol_pb2.pyi "../DummyClient"
+DEL /Q /F *.pb.h
+DEL /Q /F *pb.cc
 
 pause
