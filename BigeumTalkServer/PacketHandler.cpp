b8ea85572095d98fdce5d3b9fe6ac92dceed3743
bigeumdo <bigeumdoisland@gmail.com>
2023-04-13 20:52:42 +0900

    Feat: 패킷 처리 함수 구현 및 Protobuf 변경 사항 적용

--- BigeumTalkServer/PacketHandler.cpp
+++ BigeumTalkServer/PacketHandler.cpp
@@ -16,7 +16,6 @@ bool Handle_C_LOGIN(shared_ptr<Session>& session, Protocol::C_LOGIN& pkt)
 	if (service->UseNickname(pkt.user().nickname()) == false)
 	{
 		sPkt.set_success(false);
-		sPkt.set_serverid(0);
 
 		shared_ptr<SendBuffer> sendBuffer = PacketHandler::MakeBuffer_S_LOGIN(sPkt);
 		session->Send(sendBuffer);
@@ -24,12 +23,12 @@ bool Handle_C_LOGIN(shared_ptr<Session>& session, Protocol::C_LOGIN& pkt)
 		return true;
 	}
 
-	static atomic<unsigned long long> idGenerator = 0;
+	static atomic<unsigned long long> idGenerator = 1;
 
 	// User 객체 생성 후 Session이 참조
 	// Cycle이 형성되므로 주의
 	auto userRef = make_shared<User>();
-	userRef->userId = ++idGenerator;
+	userRef->userId = idGenerator.fetch_add(1);
 	userRef->nickname = pkt.user().nickname();
 	userRef->ownerSession = session;
 
@@ -40,7 +39,7 @@ bool Handle_C_LOGIN(shared_ptr<Session>& session, Protocol::C_LOGIN& pkt)
 #endif
 
 	sPkt.set_success(true);
-	sPkt.set_serverid(userRef->userId);
+	sPkt.set_userid(userRef->userId);
 	shared_ptr<SendBuffer> sendBuffer = PacketHandler::MakeBuffer_S_LOGIN(sPkt);
 	session->Send(sendBuffer);
 
@@ -49,18 +48,50 @@ bool Handle_C_LOGIN(shared_ptr<Session>& session, Protocol::C_LOGIN& pkt)
 
 bool Handle_C_CREATE_ROOM(shared_ptr<Session>& session, Protocol::C_CREATE_ROOM& pkt)
 {
+	auto roomManager = session->GetService()->GetRoomManager();
+
+	// 방 생성
+	unsigned long long roomId = roomManager->CreateRoom(session->_user, pkt.roomname());
+
+	// 호스트 입장
+	if (roomManager->EnterRoom(session->_user, roomId) == false)
+	{
+		// 에러 발생
+		Protocol::S_CREATE_ROOM sPkt;
+		sPkt.set_success(false);
+
+		auto sendBuffer = PacketHandler::MakeBuffer_S_CREATE_ROOM(sPkt);
+		session->Send(sendBuffer);
+
+		return true;
+	}
+
+	// 결과 패킷
+	Protocol::S_CREATE_ROOM sPkt;
+	RoomData roomData = roomManager->GetRoomData(roomId);
+	auto roomPkt = new Protocol::Room();
+	sPkt.set_success(true);
+	roomPkt->set_id(roomData.roomId);
+	roomPkt->set_roomname(roomData.roomName);
+	roomPkt->set_hostname(roomData.hostName);
+	roomPkt->set_maxuser(roomData.maxUser);
+	roomPkt->set_usercount(roomData.userCount);
+	sPkt.set_allocated_room(roomPkt);
+
+	auto sendBuffer = PacketHandler::MakeBuffer_S_CREATE_ROOM(sPkt);
+	session->Send(sendBuffer);
+
 	return true;
 }
 
 bool Handle_C_ENTER_ROOM(shared_ptr<Session>& session, Protocol::C_ENTER_ROOM& pkt)
 {
-	auto service = session->GetService();
+	auto roomManager = session->GetService()->GetRoomManager();
 	Protocol::S_ENTER_ROOM sPkt;
 
-	if (service->GetRoomManager()->EnterRoom(session->_user, pkt.roomid()) == false)
+	if (roomManager->EnterRoom(session->_user, pkt.roomid()) == false)
 	{
 		sPkt.set_success(false);
-		sPkt.set_isfail(true);
 
 		shared_ptr<SendBuffer> sendBuffer = PacketHandler::MakeBuffer_S_ENTER_ROOM(sPkt);
 		session->Send(sendBuffer);
@@ -69,17 +100,23 @@ bool Handle_C_ENTER_ROOM(shared_ptr<Session>& session, Protocol::C_ENTER_ROOM& p
 
 	// 입장 성공 - 시도한 유저에게
 	sPkt.set_success(true);
-	auto roomData = new Protocol::S_ENTER_ROOM::RoomData();
-	roomData->set_name(session->_user->room->GetRoomName());
+	auto roomPkt = new Protocol::Room();
+	RoomData roomData = roomManager->GetRoomData(pkt.roomid());
+	roomPkt->set_id(roomData.roomId);
+	roomPkt->set_roomname(roomData.roomName);
+	roomPkt->set_hostname(roomData.hostName);
+	roomPkt->set_maxuser(roomData.maxUser);
+	roomPkt->set_usercount(roomData.userCount);
+	sPkt.set_allocated_roomdata(roomPkt);
 
 	auto roomUsers = session->_user->room->GetUsersList();
 	for (auto& user : roomUsers)
 	{
-		auto roomUser = roomData->add_users();
+		auto roomUser = sPkt.add_users();
 		roomUser->set_id(user.first);
-		roomUser->set_nickname(user.second->nickname);
+		roomUser->set_nickname(user.second);
 	}
-	sPkt.set_allocated_roomdata(roomData);
+	sPkt.set_allocated_roomdata(roomPkt);
 
 	shared_ptr<SendBuffer> sendBuffer = PacketHandler::MakeBuffer_S_ENTER_ROOM(sPkt);
 	session->Send(sendBuffer);
@@ -90,11 +127,40 @@ bool Handle_C_ENTER_ROOM(shared_ptr<Session>& session, Protocol::C_ENTER_ROOM& p
 
 bool Handle_C_LEAVE_ROOM(shared_ptr<Session>& session, Protocol::C_LEAVE_ROOM& pkt)
 {
+	if (session->_user->room != nullptr)
+	{
+		return false;
+	}
+
+	auto roomManager = session->GetService()->GetRoomManager();
+
+	roomManager->LeaveRoom(session->_user);
+	Protocol::S_LEAVE_ROOM sPkt;
+	sPkt.set_success(true);
+
+	auto sendBuffer = PacketHandler::MakeBuffer_S_LEAVE_ROOM(sPkt);
+	session->Send(sendBuffer);
+
 	return true;
 }
 
 bool Handle_C_ROOM_LIST(shared_ptr<Session>& session, Protocol::C_ROOM_LIST& pkt)
 {
+	auto roomManager = session->GetService()->GetRoomManager();
+
+	Protocol::S_ROOM_LIST sPkt;
+	auto roomList = roomManager->GetRoomList();
+	sPkt.set_roomcount(roomList.size());
+	for (auto room : roomList)
+	{
+		auto roomPkt = sPkt.add_rooms();
+		roomPkt->set_id(room.roomId);
+		roomPkt->set_roomname(room.roomName);
+		roomPkt->set_hostname(room.hostName);
+		roomPkt->set_maxuser(room.maxUser);
+		roomPkt->set_usercount(room.userCount);
+	}
+
 	return true;
 }
 
